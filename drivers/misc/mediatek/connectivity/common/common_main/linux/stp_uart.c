e5dfab579684f37f579327a314e17d5d65aa6476
bengris32 <bengris32@protonmail.ch>
2022-06-19 11:56:47 +0100

    mt6785: mtk: connectivity: common: Remove buf_lock variable * This variable conflicts with another variable in a different kernel module,   and the code that uses it is commented out anyway.
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- drivers/misc/mediatek/connectivity/common/common_main/linux/stp_uart.c
+++ drivers/misc/mediatek/connectivity/common/common_main/linux/stp_uart.c
@@ -115,8 +115,8 @@ UINT8 tx_buf[MTKSTP_BUFFER_SIZE] = { 0x0 };
 
 INT32 rd_idx;
 INT32 wr_idx;
-/* struct semaphore buf_mtx; */
-spinlock_t buf_lock;
+/* struct semaphore buf_mtx;
+spinlock_t buf_lock; */
 static INT32 mtk_wcn_uart_tx(const PUINT8 data, const UINT32 size, PUINT32 written_size);
 
 
